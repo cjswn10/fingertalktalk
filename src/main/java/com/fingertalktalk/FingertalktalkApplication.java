970f0522f3873436c84af3e0b835919a28a54f1c
cjswn10 <cjswn10@gmail.com>
2017-07-04 19:34:35 +0900

    first practice

---
+++ src/main/java/com/fingertalktalk/FingertalktalkApplication.java
@@ -0,0 +1,12 @@
+package com.fingertalktalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingertalktalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FingertalktalkApplication.class, args);
+	}
+}
