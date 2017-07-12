734f326a8a7fdec8359ca0b10aebd7db27ebebb8
hyjeong <jhy83088@gmail.com>
2017-07-12 21:50:36 +0900

    [17.07.12] updateMallInfo 추가

--- src/main/java/com/fingertalktalk/controller/MallApiController.java
+++ src/main/java/com/fingertalktalk/controller/MallApiController.java
@@ -18,6 +18,9 @@ public class MallApiController {
     @Autowired
     private MallMapper mallMapper;
 
+    private Result result = new Result();
+    private Boolean bResult = false;
+
     //mall id와 일치하는 몰정보 데이턴
     @RequestMapping(method = RequestMethod.POST, value = "/getMallInfo")
     public MallVO getMallInfo(@RequestParam("mall_name") String mall_name) {
@@ -34,17 +37,55 @@ public class MallApiController {
     //몰 정보 추가
     @RequestMapping(method = RequestMethod.POST, value = "/addMallInfo")
     public Result addMallInfo(@RequestBody MallVO mallVO) {
-        Result result = new Result();
-        boolean bResult = mallMapper.addMallInfo(mallVO);
+
+        bResult = mallMapper.addMallInfo(mallVO);
         if(bResult) {
             result.setCode("0000");
             result.setMsg(mallVO.getMall_name() + " 몰이 새로 등록 되었습니다.");
+            result.setObj(mallVO);
         } else {
             System.out.println("addMallInfo 실패 " + mallVO.toString());
             result.setCode("9999");
-            result.setMsg("몰 추가 실패");
+            result.setMsg("몰정보 추가 실패");
         }
         return result;
     }
 
+    //몰 정보 업데이트
+    @RequestMapping(method = RequestMethod.POST, value = "/updateMallInfo")
+    public Result updateMallInfo(@RequestBody MallVO mallVO) {
+        mallVO = checkMallInfo(mallVO);
+        bResult = mallMapper.updateMallInfo(mallVO);
+        if(bResult) {
+            result.setCode("0000");
+            result.setMsg(mallVO.getMall_name() + "몰정보 업데이트 완료");
+            result.setObj(mallVO);
+        }
+        else {
+            System.out.println("updateMallInfo 실패 " + mallVO.toString());
+            result.setCode("9999");
+            result.setMsg("몰정보 업데이트 실패");
+        }
+        return result;
+    }
+
+    private MallVO checkMallInfo(MallVO mallVO) {
+        MallVO recentMallInfo = mallMapper.getMallInfoByIdx(mallVO.getMall_idx());
+        if(mallVO.getMall_name() == null)  {
+            mallVO.setMall_name(recentMallInfo.getMall_name());
+        }
+        if(mallVO.getServer_name() == null) {
+            mallVO.setServer_name(recentMallInfo.getServer_name());
+        }
+        if(mallVO.getMall_img() == null) {
+            mallVO.setMall_img(recentMallInfo.getMall_img());
+        }
+        if(mallVO.getAuto_login_url() == null) {
+            mallVO.setAuto_login_url(recentMallInfo.getAuto_login_url());
+        }
+        if(mallVO.getIs_cafe24() == null) {
+            mallVO.setIs_cafe24(recentMallInfo.getIs_cafe24());
+        }
+        return mallVO;
+    }
 }
