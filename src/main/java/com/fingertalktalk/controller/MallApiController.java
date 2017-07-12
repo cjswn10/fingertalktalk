cd9a848783d0546e9143fd83d071781336dd1f03
hyjeong <jhy83088@gmail.com>
2017-07-12 21:09:32 +0900

    [17.07.12] addMallInfo 추가

--- src/main/java/com/fingertalktalk/controller/MallApiController.java
+++ src/main/java/com/fingertalktalk/controller/MallApiController.java
@@ -1,12 +1,10 @@
 package com.fingertalktalk.controller;
 
 import com.fingertalktalk.domain.MallVO;
+import com.fingertalktalk.domain.Result;
 import com.fingertalktalk.persistence.MallMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,13 +20,31 @@ public class MallApiController {
 
     //mall id와 일치하는 몰정보 데이턴
     @RequestMapping(method = RequestMethod.POST, value = "/getMallInfo")
-    public MallVO getMallInfo(@RequestParam String mall_name) {
+    public MallVO getMallInfo(@RequestParam("mall_name") String mall_name) {
+        System.out.println(mall_name);
         return mallMapper.getMallInfo(mall_name);
     }
 
     //몰 리스트 데이터
-    @RequestMapping(method = RequestMethod.GET, value = "getMallList")
+    @RequestMapping(method = RequestMethod.GET, value = "/getMallList")
     public List<MallVO> getMallList() {
         return mallMapper.getMallList();
     }
+
+    //몰 정보 추가
+    @RequestMapping(method = RequestMethod.POST, value = "/addMallInfo")
+    public Result addMallInfo(@RequestBody MallVO mallVO) {
+        Result result = new Result();
+        boolean bResult = mallMapper.addMallInfo(mallVO);
+        if(bResult) {
+            result.setCode("0000");
+            result.setMsg(mallVO.getMall_name() + " 몰이 새로 등록 되었습니다.");
+        } else {
+            System.out.println("addMallInfo 실패 " + mallVO.toString());
+            result.setCode("9999");
+            result.setMsg("몰 추가 실패");
+        }
+        return result;
+    }
+
 }
