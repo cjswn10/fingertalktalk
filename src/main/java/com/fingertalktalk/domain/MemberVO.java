461414d042bb71f496d11d583102bf4d2ca7dfa4
cjswn10 <cjswn10@gmail.com>
2017-07-12 21:10:42 +0900

    [17.07.12] addMemberInfo 추가

--- src/main/java/com/fingertalktalk/domain/MemberVO.java
+++ src/main/java/com/fingertalktalk/domain/MemberVO.java
@@ -4,12 +4,12 @@ import lombok.Data;
 
 /**
  * Created by YJ on 2017-07-06.
- * 변수명은 DB의 칼럼명과 일치 시킬 것
+ * 변수명과 순서를 DB의 칼럼명과 일치 시킬 것
  */
 @Data
 public class MemberVO {
     private String member_id;
     private String member_pw;
     private String member_name;
-    private int member_phone;
+    private String member_phone;
 }
