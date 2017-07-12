35c9838eda328c646d05f0624228d44bce94e629
cjswn10 <cjswn10@gmail.com>
2017-07-12 21:10:42 +0900

    [17.07.12] addMemberInfo 추가

--- src/main/java/com/fingertalktalk/persistence/ApiMapper.java
+++ src/main/java/com/fingertalktalk/persistence/ApiMapper.java
@@ -7,6 +7,9 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.lang.reflect.Member;
 
 
 @Repository
@@ -14,13 +17,13 @@ import org.springframework.stereotype.Repository;
 public interface ApiMapper {
 
     //MemberMapper
-    @Insert("INSERT INTO member(member_id, member_pw, member_name, member_phone " +
+    @Insert("INSERT INTO member(member_id, member_pw, member_name, member_phone)" +
             "VALUES (#{member_id}, #{member_pw}, #{member_name}, #{member_phone})")
+    boolean addMemberInfo(MemberVO member);
 
+    /*@Param 안의 "member_id" 는 Controller 에서 Mapper 를 호출할때 넣어주는 파라미터와 이름이 동일해야함*/
     @Select("SELECT member_id, member_pw, member_name, member_phone from member where member_id = #{id}")
     MemberVO getMemberInfo(@Param("member_id") String member_id);
-/*@Param 안의 "id" 는 Controller에서 Mapper를 호출할때 넣어주는 파라미터와 이름이 동일해야함*/
-/*    @Select("SELECT * from member")
-    List<MemberVO> getMemberList();
-*/
+
+
 }
