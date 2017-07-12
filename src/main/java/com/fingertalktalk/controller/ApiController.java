287075149823fe17ae1756e0ac2633968829bd97
cjswn10 <cjswn10@gmail.com>
2017-07-12 22:00:15 +0900

    [17.07.12] changeMemberInfo 추가

--- src/main/java/com/fingertalktalk/controller/ApiController.java
+++ src/main/java/com/fingertalktalk/controller/ApiController.java
@@ -23,11 +23,25 @@ public class ApiController {
     @RequestMapping(method = RequestMethod.POST, value = "/addMemberInfo")
     public String addMemberInfo(@RequestBody MemberVO member) {
         Boolean result = apiMapper.addMemberInfo(member);
-
         if(result)
             return member.getMember_name() + "님 회원가입되었습니다.";
         else
             return "회원가입에 실패했습니다.";
     }
 
-}
+    @RequestMapping(method = RequestMethod.POST, value = "/changeMemberInfo")
+    public String changeMemberInfo(@RequestBody MemberVO member) {
+        MemberVO origin = apiMapper.getMemberInfo(member.getMember_id());
+        if(member.getMember_pw() == null)
+            member.setMember_pw(origin.getMember_pw());
+        if(member.getMember_name() == null)
+            member.setMember_name(origin.getMember_name());
+        if(member.getMember_phone() == null)
+            member.setMember_phone(origin.getMember_phone());
+
+        Boolean result = apiMapper.changeMemberInfo(member);
+        if(result)
+            return "회원정보가 수정되었습니다.";
+        else
+            return "회원정보가 수정되지 않았습니다.";
+    }}
